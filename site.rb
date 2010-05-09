8e0799775fe20061b1575b95a689b08ee12594e1
Luke Sutton <luke.sutton@gmail.com>
2010-05-09 15:23:15 +0930

    Remember to use relative links in stylesheets.

--- site.rb
+++ site.rb
@@ -1,6 +1,6 @@
 Pekky.site do
   set :title, "Pekky!"
-  set :path_prefix, "pekky"
+  set :path_prefix, "/pekky"
   
   page '/', 'index', :view => 'index'
   page '/getting-started', 'getting_started'
