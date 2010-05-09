e08007df4ae4f83eb847362be07a07ecd778d2b0
Luke Sutton <luke.sutton@gmail.com>
2010-05-09 20:32:32 +0930

    Document the shared content.

--- site.rb
+++ site.rb
@@ -6,11 +6,12 @@ Pekky.site do
   page '/getting-started', 'getting_started'
   
   # References
-  page '/reference',            'reference/list', :view => 'reference_list'
-  page '/reference/templates',  'reference/templates'
-  page '/reference/helpers',    'reference/helpers'
-  page '/reference/generating', 'reference/generating'
-  page '/reference/console',    'reference/console'
-  page '/reference/models',     'reference/models'
-  page '/reference/markup',     'reference/markup'
+  page '/reference',                'reference/list', :view => 'reference_list'
+  page '/reference/templates',      'reference/templates'
+  page '/reference/helpers',        'reference/helpers'
+  page '/reference/generating',     'reference/generating'
+  page '/reference/console',        'reference/console'
+  page '/reference/models',         'reference/models'
+  page '/reference/markup',         'reference/markup'
+  page '/reference/shared-content', 'reference/shared_content'
 end
